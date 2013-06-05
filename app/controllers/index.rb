42251c5021d45097e5c961fde946379d50c502a7
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 16:46:57 -0500

    first commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,9 +4,39 @@ get '/' do
 
 end
 
-post '/new_user' do
-  
+post '/create' do
+  new_user = User.create(params[:user])
+  session[:user_id] = new_user.id
+  redirect '/secret_page'
+
 end
+
+post '/login' do
+  @user = User.authenticate(params[:user])
+  if @user
+     session[:user_id] = @user.id
+    erb :secret_page
+  else
+    redirect to '/'
+  end
+end
+
+get '/secret_page' do
+  if current_user
+    erb :secret_page
+  else 
+    redirect '/'
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to '/'
+end
+
+
+
+
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,26 @@
 class User < ActiveRecord::Base
   # e.g., User.authenticate('jesse@devbootcamp.com', 'apples123')
-  def self.authenticate(email, password)
-    # if email and password correspond to a valid user, return that user
-    # otherwise, return nil
+
+
+  def self.authenticate(params)
+    user = User.find_by_email(params[:email])
+    (user && user.password == params[:password]) ? user : nil
   end
 
-  
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.create(params)
+    p @user = User.new(params)
+    p @user.password = params[:password]
+    @user.save!
+    @user
+  end
+
 end
